da21cc2afbd2d958aabfdfb346c4de1556479b19
ahrisjakkmatt <76171602+ahrisjakkmatt@users.noreply.github.com>
2021-02-06 00:09:39 -0300

    Completed Project 2, with unittest passing all combinations of flags

--- count_b.py
+++ count_b.py
@@ -4,8 +4,8 @@
 #                       from flags
 
 import sys
-import csv
-import os
+# import csv
+# import os
 import collections
 
 # Create alphabet list of lowercase letters
@@ -28,7 +28,8 @@ for letter in alphabetLower:
 
 # -l option:        restrict what characters are printed.
 # -c option:        make the counting case-sensitive. When omitted, defaults to being case-insensitive.
-# -z option:        print out chars that occurred with a frequency of zero. If omitted, chars with a frequency of zero are not printed.
+# -z option:        print out chars that occurred with a frequency of zero. If omitted,
+# chars with a frequency of zero are not printed.
 
 # Ignore Case
 # Count multiple files
@@ -45,116 +46,122 @@ def merge(dict_1, dict_2):
     b_counter = collections.Counter(dict_2)
     add_dict = a_counter + b_counter
     dict_3 = dict(add_dict)
-    return(dict_3)
-'''
-def recursiveAdd(list):
-    rates = {}
-    for d, value in list:
-        merge(rates, list[d])
-        if value == list[-1]:
-            print(rates)
-            return rates
-        print(rates)
-        return rates + recursiveAdd(rates,list[i:])'''
+    return dict_3
+
 
 def flags():
     """"This function parses flags from the command line arguments"""
-    queryLetters = ""
-    flags = set()
-
-    fileList = []
+    query_letters = ""
+    flag_set = set()
+    all_flags = []
+    file_list = []
     """parse the file names"""
     for i in range(1, len(sys.argv)):
         if '.txt' in sys.argv[i]:
-            fileList.append(sys.argv[i])
+            file_list.append(sys.argv[i])
 
     for i in range(1, len(sys.argv)):
         if '-' in sys.argv[i]:
-            flags.add(sys.argv[i][1:])
+            flag_set.add(sys.argv[i][1:])
             if '-l' in sys.argv[i]:
-                queryLetters = sys.argv[i + 1]
-    total_flags = [flags, queryLetters, fileList]
-    print(total_flags)
-    return (total_flags)
+                query_letters = sys.argv[i + 1]
+    all_flags.append([flag_set, query_letters, file_list])
+    print(all_flags)
+    return all_flags
 
-def w_to_csv(filename, dict):
+def w_to_csv(filename, dictionary):
+    """function takes a filename, and a dictionaryionary and prints to the file the dictionary in csv format"""
     csv_file = filename
     with open(csv_file, 'w') as f:
-        for key in dict.keys():
-            f.write("%s,%s\n" % (key, dict[key]))
+        for key in dictionary.keys():
+            f.write("%s,%s\n" % (key, dictionary[key]))
 
-def add_frequencies(d, file, remove_case, flagSet, queryLetters):
+def add_freq(d, file, remove_case, flag_set, query_letters):
     """a dictionary d, a file object file, and a boolean remove_case (from -c)"""
     file = open(file, mode='r', encoding='ASCII')
     fullText = file.read()
     """the following line takes care of -c"""
-    if remove_case == True:
+    if remove_case is True:
         fullText = fullText.lower()
-    if "z" in flagSet:
+    if "z" in flag_set:
         """create a dictionary with values of 0 prior to counting"""
-        if remove_case == True:
+        if remove_case is True:
             for letter in alphabetLower:
                 d[letter] = 0
         else:
             for letter in alphabetCombined:
                 d[letter] = 0
 
-    for letter in fullText:
-        if "l" in flagSet:
-            if letter in queryLetters:
-                """flagsLetters[1] is the set of letters from which to limit when -l is called"""
+    if "l" in flag_set:
+        for letter in fullText:
+            if letter in query_letters:
                 if letter in d:
+                    """flagsLetters[1] is the set of letters from which to limit when -l is called"""
                     d[letter] = d[letter] + 1
                 else:
                     d[letter] = 1
-        else:
+    else:
+        for letter in fullText:
             if letter in alphabetCombined:
+                """only select the char within the alphabet, excluding punctuations"""
                 if letter in d:
                     d[letter] = d[letter] + 1
                 else:
                     d[letter] = 1
-    return(d)
+    return d
+
 
 def returnDict(totalFlags):
-    totalletterFrequencies = {}
+    totl_letter_frenquencies = {}
     """"Create an empty dictionary"""
-    fileList = totalFlags[2]
-    dictionaryList = []
-    for i in range(len(fileList)):
-            letterFrequencies = {}
-            file = fileList[i]
-            print()
-            print(f'File name: {file}')
-            if "c" in totalFlags[0]:
-                """provide count of letters within the file"""
-                letterFrequencies = add_frequencies(letterFrequencies, file, False, totalFlags[0], fileList)
-                dictionaryList.append(letterFrequencies)
-                """provide count of letters as a running total"""
-                totalletterFrequencies = add_frequencies(letterFrequencies, file, False, totalFlags[0], fileList)
-            else:
-                letterFrequencies = add_frequencies(letterFrequencies, file, True, totalFlags[0], fileList)
-                dictionaryList.append(letterFrequencies)
-                totalletterFrequencies = add_frequencies(letterFrequencies, file, True, totalFlags[0], fileList)
-            print(dictionaryList[i])
-    frenquencyList = []
-    if len(dictionaryList) == 1:
-        totalletterFrequencies = dictionaryList[0]
-    elif len(dictionaryList) == 2:
-        totalletterFrequencies = merge(dictionaryList[0],dictionaryList[1])
-    elif len(dictionaryList) == 3:
-        temp = merge(dictionaryList[0],dictionaryList[1])
-        totalletterFrequencies = merge(temp, dictionaryList[2])
+    file_list = totalFlags[2]
+    dictionary_list = []
+    for i in range(len(file_list)):
+        letterFrequencies = {}
+        file = file_list[i]
+        print(f'File name: {file}')
+        if "c" in totalFlags[0]:
+            """provide count of letters within the file"""
+            letterFrequencies = add_freq(letterFrequencies, file, False, totalFlags[0], totalFlags[1])
+            dictionary_list.append(letterFrequencies)
+            """provide count of letters as a running total"""
+            totl_letter_frenquencies = add_freq(totl_letter_frenquencies, file, False, totalFlags[0], totalFlags[1])
+        else:
+            letterFrequencies = add_freq(letterFrequencies, file, True, totalFlags[0], totalFlags[1])
+            dictionary_list.append(letterFrequencies)
+            totl_letter_frenquencies = add_freq(totl_letter_frenquencies, file, True, totalFlags[0], totalFlags[1])
+        print(dictionary_list[i])
+
+    if len(dictionary_list) == 1:
+        totl_letter_frenquencies = dictionary_list[0]
+    elif len(dictionary_list) == 2:
+        totl_letter_frenquencies = merge(dictionary_list[0], dictionary_list[1])
+    elif len(dictionary_list) == 3:
+        temp = merge(dictionary_list[0], dictionary_list[1])
+        totl_letter_frenquencies = merge(temp, dictionary_list[2])
     else:
         print("ERROR: There is more than 3 .txt files.")
+    emptyDict = {}
+    if ('z' in totalFlags[0]) and ('c' in totalFlags[0]):
+        for letter in alphabetCombined:
+            emptyDict[letter] = 0
+        totl_letter_frenquencies = emptyDict | totl_letter_frenquencies
+    elif ('z' in totalFlags[0]) and ('c' not in totalFlags[0]):
+        for letter in alphabetLower:
+            emptyDict[letter] = 0
+        totl_letter_frenquencies = emptyDict | totl_letter_frenquencies
+
     print()
     print(f'Total Frequencies:')
-    print(totalletterFrequencies)
-    w_to_csv("out.csv",totalletterFrequencies)
+    print(totl_letter_frenquencies)
+    w_to_csv("out.csv", totl_letter_frenquencies)
     """Add the frequencies for each file in the argument list to the dictionary"""
+    return totl_letter_frenquencies
+
 
 # ---------------------------------------
-total_flags = flags()
+all_flags = flags()
 
 #
 if __name__ == "__main__":
-    returnDict(total_flags)
+    returnDict(all_flags)
--- test_count_b.py
+++ test_count_b.py
@@ -1,65 +1,66 @@
 import unittest
 import count_b
-#import count_to_csv
-#import csv
-
-
-'''
-from count_b import returnDict
-emptyDict = {}
-returnDict()
-
-class flags:
-    def __init__(self, files=[''], flags={}, queryLetters=''):
-        self.files = files
-        self.flagSet = flags
-        self.queryLetters = queryLetters
-'''
 
 class TestCount(unittest.TestCase):
     def test_returnDict(self):
-        noFlags = [{},'', 'test.txt']
+        noFlags = [set(),'', ['test.txt','file_1.txt','file_2.txt']]
         result_noFlags = count_b.returnDict(noFlags)
-        flags_C = [{'c'}, '', ['test.txt']]
+        flags_C = [{'c'}, '', ['test.txt', 'file_1.txt','file_2.txt']]
         result_C = count_b.returnDict(flags_C)
-        flags_L = [{'l'}, 'ab', ['test.txt']]
+        flags_L = [{'l'}, 'ab', ['test.txt', 'file_1.txt', 'file_2.txt']]
         result_L = count_b.returnDict(flags_L)
-        flags_Z = [{'z'}, '', ['test.txt']]
+        flags_Z = [{'z'}, '', ['test.txt','file_1.txt','file_2.txt']]
         result_Z = count_b.returnDict(flags_Z)
-        flags_CL = [{'c','l'}, '', ['file_1.txt', 'file_2.txt']]
+        flags_CL = [{'c','l'}, 'Ahriel', ['test.txt', 'file_1.txt', 'file_2.txt']]
         result_CL = count_b.returnDict(flags_CL)
-        flags_CZ = [{'c','z'}, '', ['file_1.txt', 'file_2.txt']]
+        flags_CZ = [{'c','z'}, '', ['test.txt','file_1.txt', 'file_2.txt']]
         result_CZ = count_b.returnDict(flags_CZ)
-        flags_LZ = [{'l', 'z'}, '', ['file_1.txt', 'file_2.txt']]
+        flags_LZ = [{'l', 'z'}, 'Ahriel', ['file_1.txt', 'file_2.txt', 'test.txt']]
         result_LZ = count_b.returnDict(flags_LZ)
         flags_CLZ = [{'c','l','z'}, 'Ahriel', ['file_1.txt', 'file_2.txt', 'test.txt']]
         result_CLZ = count_b.returnDict(flags_CLZ)
 
         self.assertEqual(result_noFlags,
-                         {"a": 2,"b":2,"c":2,"d":2})
+                         #return all letter counts after converting all letters to lowercase
+                         {'a': 3, 'b': 2, 'c': 4, 'd': 4, 'h': 4, 'e': 6, 'l': 6, 'o': 5, 'w': 2, 'r': 1, 't': 5, 'i': 7, 's': 6, 'f': 1, 'n': 5})
         self.assertEqual(result_C,
-                         {"b":2,"c":1,"d":1,"A":2,"C":1,"D":1})
+                         #return all letters without changing capitalization
+                         {'A': 2, 'b': 2, 'C': 1, 'c': 3, 'd': 3, 'D': 1, 'H': 1, 'e': 6, 'l': 6, 'o': 5, 'w': 2,
+                          'r': 1, 'T': 2, 'h': 3, 'i': 7, 's': 6, 'f': 1, 'n': 5, 't': 3, 'a': 1})
         self.assertEqual(result_L,
-                         {"a":2,"b":2})
+                         #only looking for a and b
+                         {'a': 3, 'b': 2})
         self.assertEqual(result_Z,
-                         {"a":2, "b":2,"c":2,"d":2,"e":0,"f":0,"g":0,"h":0,"i":0,"j":0,"k":0,"l":0,"m":0,"n":0,
-                          "o":0,"p":0,"q":0,"r":0,"s":0,"t":0,"u":0,"v":0,"w":0,"x":0,"y":0,"z":0})
+                         #these are all letters in the lower alphabet
+                         {'a': 3, 'b': 2, 'c': 4, 'd': 4, 'e': 6, 'f': 1, 'g': 0, 'h': 4, 'i': 7, 'j': 0, 'k': 0,
+                          'l': 6, 'm': 0, 'n': 5, 'o': 5, 'p': 0, 'q': 0, 'r': 1, 's': 6, 't': 5, 'u':
+                              0, 'v': 0, 'w': 2, 'x': 0, 'y': 0, 'z': 0})
         self.assertEqual(result_CL,
-                         {'H': 1, 'e': 6, 'l': 6, 'o': 5, 'w': 2, 'r': 1, 'd': 2, 'T': 2, 'h': 3, 'i': 7, 's': 6,
-                          'f': 1, 'c': 2, 'n': 5, 't': 3, 'a': 1})
+                         #looking for the letters in the string Ahriel without changing capitalization
+                         {'A': 2, 'e': 6, 'l': 6, 'r': 1, 'h': 3, 'i': 7})
         self.assertEqual(result_CZ,
-                         {'H': 1, 'e': 6, 'l': 6, 'o': 5, 'w': 2, 'r': 1, 'd': 2, 'T': 2, 'h': 3, 'i': 7, 's': 6,
-                          'f': 1, 'c': 2, 'n': 5, 't': 3, 'a': 1})
+                         #these are all letters in the combined upper and lower alphabets
+                         {'A': 2, 'B': 0, 'C': 1, 'D': 1, 'E': 0, 'F': 0, 'G': 0, 'H': 1, 'I': 0, 'J': 0, 'K': 0,
+                          'L': 0, 'M': 0, 'N': 0, 'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 2, 'U':
+                              0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0, 'a': 1, 'b': 2, 'c': 3, 'd': 3, 'e': 6, 'f': 1,
+                          'g': 0, 'h': 3, 'i': 7, 'j': 0, 'k': 0, 'l': 6, 'm': 0, 'n': 5, 'o': 5, 'p'
+                          : 0, 'q': 0, 'r': 1, 's': 6, 't': 3, 'u': 0, 'v': 0, 'w': 2, 'x': 0, 'y': 0, 'z': 0})
         self.assertEqual(result_LZ,
-                         {'H': 1, 'e': 6, 'l': 6, 'o': 5, 'w': 2, 'r': 1, 'd': 2, 'T': 2, 'h': 3, 'i': 7, 's': 6,
-                          'f': 1, 'c': 2, 'n': 5, 't': 3, 'a': 1})
+                         #within the given string 'Ahriel' what letters matches the lowercase letters ... 'hriel' and give 0 to all others
+                         #this is somewhat contradictory, confusing, redundant and not necessarily useful feedback
+
+                         {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 6, 'f': 0, 'g': 0, 'h': 4, 'i': 7, 'j': 0, 'k': 0,
+                          'l': 6, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 1, 's': 0, 't': 0, 'u':
+                              0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0})
         self.assertEqual(result_CLZ,
-                         {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 0, 'I': 0, 'J': 0, 'K': 0,
+                         #sampling out of the combined upper and lower alphabets, count the correct case and letter match within the string 'Ahriel'
+                         #providing 0 to all other entries
+
+                         {'A': 2, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 0, 'I': 0, 'J': 0, 'K': 0,
                           'L': 0, 'M': 0, 'N': 0, 'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0, 'U':
-                              0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0, 'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0,
-                          'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p'
-                          : 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0}
-                         )
+                              0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0, 'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 6, 'f': 0,
+                          'g': 0, 'h': 3, 'i': 7, 'j': 0, 'k': 0, 'l': 6, 'm': 0, 'n': 0, 'o': 0, 'p'
+                          : 0, 'q': 0, 'r': 1, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0})
 
 
 if __name__ == '__main__':
