ec45b182f6564cef25b24c0c27ac4b0201e3530e
ahrisjakkmatt <76171602+ahrisjakkmatt@users.noreply.github.com>
2021-03-12 01:37:01 -0300

    zip_card with comments

--- Project 5/zip_cards.py
+++ Project 5/zip_cards.py
@@ -6,9 +6,11 @@ values = ["Ace","King","Queen","Jack","Ten","Nine","Eight","Seven","Six","Five",
 deck = []
 
 for suit in suits:
-    temp = []
+    temp = [] # create a temporary list, from which will create tuples
     temp.append(suit)
     deck.extend(list(zip_longest(temp, values, fillvalue=suit)),)
+    # zip through the longest list i.e. values, and join with suit, filling in the suit for None
+    # it is necessary to extend in order to not create multiple tuples or lists
 
 print(deck)
 
